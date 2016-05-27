7249c7dce0652fe37934e658d05213d5448be698
wangtao1991 <493794932@qq.com>
2016-05-27 22:24:35 +0800

    第一周作业

---
+++ 第三组王涛/第一周作业/王涛/BufferConcat.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+var buffer1=new Buffer('诗和');
+var buffer2=new Buffer('远方');
+
+Buffer.concat1= function (list, len) {
+    if(list.length==1){
+        return list[0];
+    }
+    if(!len){
+        len=0;
+        for(var i= 0;i<list.length;i++){
+            len+=list[i].length;
+        }
+    }
+    var curLen=0;
+    var newBuffer=new Buffer(len);
+    for(i=0;i<list.length;i++){
+        var cur=list[i];
+        cur.copy(newBuffer,curLen);
+        curLen+=cur.length;
+    }
+    return newBuffer.slice(0,len);
+};
+
+console.log(Buffer.concat1([buffer1,buffer2]).toString());
