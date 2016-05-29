8ce32770c1efeb218878312a09c101feaea5d67d
jipings <876019545@qq.com>
2016-05-30 00:17:22 +0800

    zuoye

--- 第三组王涛/第一周作业/刘纪平/第一周作业/事件.js
+++ 第三组王涛/第一周作业/刘纪平/第一周作业/事件.js
@@ -1,47 +1,4 @@
 
-function processThis(callBack, context) {
-    var outerArg = Array.prototype.slice.call(arguments, 2);
-    return function () {
-        var innerArg = Array.prototype.slice.call(arguments, 0);
-        callBack.apply(context, outerArg.concat(innerArg));
-    }
-}
-function bind(curEle, type, fn) {
-    if ("addEventListener" in curEle) {
-        curEle.addEventListener(type, fn, false);
-        return;
-    }
-    var tempFn = processThis(fn, curEle);
-    tempFn.photo = fn;
-    if (!curEle["myBind" + type]) {
-        curEle["myBind" + type] = [];
-    }
-    var ary = curEle["myBind" + type];
-    for (var i = 0; i < ary.length; i++) {
-        if (ary[i].photo === fn) {
-            return;
-        }
-    }
-    ary.push(tempFn);
-    curEle.attachEvent("on" + type, tempFn);
-}
-function unbind(curEle, type, fn) {
-    if ("removeEventListener" in curEle) {
-        curEle.removeEventListener(type, fn, false);
-        return;
-    }
-    var ary = curEle["myBind" + type];
-    if (ary) {
-        for (var i = 0; i < ary.length; i++) {
-            var tempFn = ary[i];
-            if (tempFn.photo === fn) {
-                curEle.detachEvent("on" + type, tempFn);
-                ary.splice(i, 1);
-                break;
-            }
-        }
-    }
-}
 function on(curEle, type, fn) {
     if (!curEle["myEvent" + type]) {
         curEle["myEvent" + type] = [];
